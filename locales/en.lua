6dff5f36c4fed1a7e2158ec016d8b00c2fa02cff
mrkuko <59928225+mrkuko@users.noreply.github.com>
2021-01-31 19:57:30 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,24 @@
+Locales["en"] = {
+	--client
+	["respawn"] = "type ~u~/respawn~s~ for fresh start",
+	["enter_race"] = "Press ~INPUT_PICKUP~ to enter race event!",
+	["exit"] = "type ~u~/exit~s~ to exit race",
+	["announcement"] = "Hello, this is MKR (mr__kuko's racing)",
+	["go"] = "GO!",
+	["finish_line"] = "Race Finished!",
+	["total_time"] = "~h~total time: ",
+	["total_time_chat"] = "Total time: ",
+	--server
+	["error_setmoney"] = "error in amount of money to be setted",
+	["error_paid"] = "You don't have enough money to enter the race.",
+	---notifications
+	["race_entry"] = "Race Entry",
+	["race_entry_header"] = "Thanks for your entry",
+	["race_entry_message"] = "Now show me what u can do! ~n~MONEY: ~r~-",
+	["race_reward"] = "Race Reward",
+	["race_reward_header"] = "Congratulations, man",
+	["race_reward_message"] = "Today's your lucky. Grab this money sooner then i'll kick your ass ~n~MONEY: ~g~",
+	["race_failed"] = "Race Event",
+	["race_failed_header"] = "O, that's unlucky",
+	["race_failed_message"] = "Luckly i'm not u. I don't know what u even expected... Bye looser!"
+}
---
+++ locales/fr.lua
@@ -0,0 +1,24 @@
+Locales["en"] = {
+	--client
+	["respawn"] = "Entrer ~u~/respawn~s~ pour un nouveau départ",
+	["enter_race"] = "Entrer ~INPUT_PICKUP~ pour participer à l'événement de course!",
+	["exit"] = "Entrer ~u~/exit~s~ pour quitter la course",
+	["announcement"] = "Bonjour, c'est MKR (mr__kuko's racing)",
+	["go"] = "ALLER!",
+	["finish_line"] = "Course fini!",
+	["total_time"] = "~h~temps total: ",
+	["total_time_chat"] = "Temps total: ",
+	--server
+	["error_setmoney"] = "erreur dans quantité d'argent ajoutée",
+	["error_paid"] = "Vous n'avez pas assez d'argent pour participer à la course.",
+	---notifications
+	["race_entry"] = "Inscription à la course",
+	["race_entry_header"] = "Merci pour votre entrée",
+	["race_entry_message"] = "Maintenant, montre-moi ce que tu peux faire! ~n~MONEY: ~r~-",
+	["race_reward"] = "Récompense de course",
+	["race_reward_header"] = "Félicitations, mec",
+	["race_reward_message"] = "Aujourd'hui, vous nous avez montré ce qu'est vraiment la vitesse. ~n~MONEY: ~g~",
+	["race_failed"] = "Événement de course",
+	["race_failed_header"] = "Mauvaise nouvelle...",
+	["race_failed_message"] = "Essayez d'être plus rapide."
+}
